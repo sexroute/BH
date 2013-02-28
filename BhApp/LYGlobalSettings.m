57651ec4155d913c09fe4ed646f7f97ca71770e6
sexroute <zhaodali78@gmail.com>
2013-02-28 17:35:21 +0800

    [ADD] 部分增加sqlite3支持

--- BhApp/LYGlobalSettings.m
+++ BhApp/LYGlobalSettings.m
@@ -10,8 +10,11 @@
 
 @implementation LYGlobalSettings
 
-
+static sqlite3 *contactDB = nil;
 static NSMutableDictionary * g_pSettingsDic = nil;
+static NSArray *dirPaths = nil;
+static NSString *docsDir = nil;
+static NSString * databasePath = nil;
 + (void)InitSetting
 {
     @synchronized(self)
@@ -23,6 +26,40 @@ static NSMutableDictionary * g_pSettingsDic = nil;
 
     }
    
+}
+
++ (void)InitDatabase
+{
+      
+    // Get the documents directory
+    dirPaths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    
+    docsDir = [dirPaths objectAtIndex:0];
+    
+    // Build the path to the database file
+    databasePath = [[NSString alloc] initWithString: [docsDir stringByAppendingPathComponent: @"contacts.db"]];
+    
+    NSFileManager *filemgr = [NSFileManager defaultManager];
+    
+    if ([filemgr fileExistsAtPath:databasePath] == NO)
+    {
+        const char *dbpath = [databasePath UTF8String];
+        if (sqlite3_open(dbpath, &contactDB)==SQLITE_OK)
+        {
+            char *errMsg;
+            const char *sql_stmt = "CREATE TABLE IF NOT EXISTS SETTING(ID TEXT PRIMARY KEY , VAL TEXT)";
+            if (sqlite3_exec(contactDB, sql_stmt, NULL, NULL, &errMsg)!=SQLITE_OK) {
+                NSLog(@"创建表失败\n");
+            }
+            
+            sqlite3_close(contactDB);
+        }
+        else
+        {
+            NSLog(@"创建/打开数据库失败"); ;
+        }
+    }
+
 }
 
 +(NSString *) GetSetting:(NSString *) apKey
@@ -50,18 +87,23 @@ static NSMutableDictionary * g_pSettingsDic = nil;
 }
 + (void) initDict
 {
-    [g_pSettingsDic setObject:@"http://bhxz808.3322.org:8090" forKey:(SETTING_KEY_SERVER_ADDRESS)];
-    [g_pSettingsDic setObject:@"222.199.224.145" forKey:(SETTING_KEY_MIDDLE_WARE_IP)];
-    [g_pSettingsDic setObject:@"7005" forKey:(SETTING_KEY_MIDDLE_WARE_PORT)];
+    @synchronized(self)
+    {
+        
+      [LYGlobalSettings InitDatabase];
+      [g_pSettingsDic setObject:@"http://bhxz808.3322.org:8090" forKey:(SETTING_KEY_SERVER_ADDRESS)];
+      [g_pSettingsDic setObject:@"222.199.224.145" forKey:(SETTING_KEY_MIDDLE_WARE_IP)];
+      [g_pSettingsDic setObject:@"7005" forKey:(SETTING_KEY_MIDDLE_WARE_PORT)];
 
 //    [g_pSettingsDic setObject:@"http://192.168.12.100:8080" forKey:(SETTING_KEY_SERVER_ADDRESS)];
 //    [g_pSettingsDic setObject:@"192.168.123.213" forKey:(SETTING_KEY_MIDDLE_WARE_IP)];
 //    [g_pSettingsDic setObject:@"7001" forKey:(SETTING_KEY_MIDDLE_WARE_PORT)];
     
     
-    [g_pSettingsDic setObject:@"" forKey:(SETTING_KEY_USER)];
-    [g_pSettingsDic setObject:@"" forKey:(SETTING_KEY_PASSWORD)];
-    [g_pSettingsDic setObject:@"1" forKey:(SETTING_KEY_SERVERTYPE)];
+      [g_pSettingsDic setObject:@"" forKey:(SETTING_KEY_USER)];
+      [g_pSettingsDic setObject:@"" forKey:(SETTING_KEY_PASSWORD)];
+      [g_pSettingsDic setObject:@"1" forKey:(SETTING_KEY_SERVERTYPE)];
+    }
 }
 - (id)init
 {
